1f27e71fa5a3724200c97449a9ae815e828a88f3
Малаховский Игорь <megazord29@yandex.ru>
2023-02-09 21:32:51 +0300

    Спринт 3. Тема 4. Добавил шаблоны и статику

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    intro = "Это главная страница проекта Yatube"
+    context = {
+        "intro": intro
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+    template = 'posts/group_posts.html'
+    intro = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        "intro": intro,
+    }
+    return render(request, template, context)
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    intro = "Список групп проекта"
+    context = {
+        "intro": intro,
+    }
+    return render(request, template, context)
 
 # Create your views here.
